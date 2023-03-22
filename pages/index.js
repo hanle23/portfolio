df8e6fad63285cba6d8adbb590006f9b837c2b5a
Han Le <hanle.cs23@gmail.com>
2023-03-21 20:27:06 -0400

    Add example of dynamic import

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import dynamic from "next/dynamic";
 
+const DynamicAbout = dynamic(() => import("./about"), {
+  loading: () => <p>Loading...</p>,
+});
 Home.title = "Homepage";
 export default function Home() {
   return (
     <div>
       <main className="h-screen w-screen flex flex-col items-center justify-center  bg-white dark:bg-black">
         <h1 className="text-3xl font-bold text-blue-500 dark:text-red-500">
-          Hello world!
+          <DynamicAbout />
         </h1>
       </main>
     </div>
