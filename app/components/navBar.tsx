a631a615c838b5e0ffe9ad5408a4cec10529355a
Han Le <hanle.cs23@gmail.com>
2023-09-13 10:15:44 -0400

    Add navBar

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -7,8 +7,12 @@ export default function NavBar(): React.JSX.Element {
     <div className="flex justify-center space-x-44">
       {route.map((routePath: string) => {
         return (
-          <Link href={`/${routePath}`} className="w-1/12" key={routePath}>
-            <button className="text-white text-base font-extrabold h-full w-full transition duration-150 bg-transparent p-2.5 hover:scale-110 rounded-lg hover:shadow-md hover:cursor-none hover:bg-white hover:bg-opacity-10">
+          <Link
+            href={`/${routePath}`}
+            className="w-1/12 hover:cursor-none"
+            key={routePath}
+          >
+            <button className="text-white text-base font-extrabold h-full w-full transition duration-150 bg-transparent p-2.5 hover:scale-110 rounded-lg hover:shadow-md hover:bg-white hover:bg-opacity-10">
               <p>{routePath.charAt(0).toUpperCase() + routePath.slice(1)}</p>
             </button>
           </Link>
