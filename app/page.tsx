bdd576f0234e7e0693d6f309d23b8436a24261e5
Han Le <73045899+hanle23@users.noreply.github.com>
2023-09-13 21:16:05 +0700

    11 implement context cursor (#13)
    * remove button library and emotion style library
    
    * Added hover effect to nav bar elements
    
    * Add navBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,13 @@
 'use client'
-import Link from 'next/link'
-import RerouteButton from './components/rerouteButton'
+import NavBar from './components/navBar'
 import React from 'react'
 import GreetingPhrase from './components/greetingPhrase'
 
 export default function Page(): JSX.Element {
   return (
-    <div className="lg:flex overscroll-none items-center w-full h-screen bg-gradient-to-tr to-blue-400 from-green-500 p-10">
-      <div className="h-full w-1/6">
-        <div className="flex h-full sticky overflow-auto lg:space-y-3  lg:flex-col pt-14">
-          <Link href="/experience">
-            <RerouteButton text={'Experience'}></RerouteButton>
-          </Link>
-          <Link href="/portfolio">
-            <RerouteButton text={'Portfolio'}></RerouteButton>
-          </Link>
-          <Link href="/contact">
-            <RerouteButton text={'Contact'}></RerouteButton>
-          </Link>
-        </div>
-      </div>
-      <div className="grid content-center justify-items-start h-full w-full">
+    <div className="overscroll-none items-center w-full h-screen bg-gradient-to-tr to-blue-400 from-green-500 px-10 pt-3">
+      <NavBar />
+      <div className="grid content-center justify-items-start w-full pl-32 pt-40">
         <GreetingPhrase />
       </div>
     </div>
