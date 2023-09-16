f4b3bb8ff30563e39ab2f3c0dd37915e5e7d7af6
Han Le <hanle.cs23@gmail.com>
2023-09-15 23:48:23 -0400

    Add Back function to navbar

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
 export default function NavBar(): React.JSX.Element {
-  const route = ['experience', 'projects', 'contact']
+  const pathname = usePathname()
+  const [route, setRoute] = useState<string[]>([])
+  useEffect(() => {
+    const url = `${pathname}`
+    const originalRoute: string[] = ['experience', 'projects', 'contact']
+    const nextRoute: string[] = originalRoute.map((v, _) => {
+      if (v === url.replace('/', '')) {
+        return 'home'
+      } else {
+        return v
+      }
+    })
+    setRoute(nextRoute)
+  }, [pathname])
 
   return (
     <div className="flex justify-center space-x-44">
-      {route.map((routePath: string) => {
+      {route?.map((routePath: string) => {
         return (
           <Link
-            href={`/${routePath}`}
+            href={`/${routePath === 'home' ? '' : routePath}`}
             className="w-1/12 hover:cursor-none"
             key={routePath}
           >
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 'use client'
-import NavBar from './components/navBar'
 import React from 'react'
 import GreetingPhrase from './components/greetingPhrase'
 
 export default function Page(): JSX.Element {
   return (
-    <div className="overscroll-none items-center w-full h-screen bg-gradient-to-tr to-blue-400 from-green-500 px-10 pt-3">
-      <NavBar />
-      <div className="grid content-center justify-items-start w-full pl-32 pt-40">
-        <GreetingPhrase />
-      </div>
+    <div className="grid content-center justify-items-start w-full pl-32 pt-40">
+      <GreetingPhrase />
     </div>
   )
 }
