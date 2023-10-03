eb5484358b76d65848ad2dfd663a86e25a3abcf7
Han Le <hanle.cs23@gmail.com>
2023-10-03 08:13:35 -0400

    Add data holder

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { experiencesData } from '@/lib/data'
 export default function Page(): React.JSX.Element {
   return (
     <div className="grid content-center justify-items-start w-full pt-20 lg:pt-40">
