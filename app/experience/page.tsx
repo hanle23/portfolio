1f42c4196890c62b242408cd3800082ddc04d537
Han Le <hanle.cs23@gmail.com>
2023-10-04 09:22:59 -0400

    Solved sticky issues

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -2,8 +2,110 @@ import React from 'react'
 import { experiencesData } from '@/lib/data'
 export default function Page(): React.JSX.Element {
   return (
-    <div className="grid content-center justify-items-start w-full pt-20 lg:pt-40">
-      Testing
+    <div>
+      <h2 className="text-center text-sky-100 font-extrabold text-lg md:text-4xl my-8">{`Experiences`}</h2>
+      <article className="flex">
+        <div>
+          <div className="sticky top-10">Category Header</div>
+          <div className="sticky -z-[1] bottom-[100px]" />
+        </div>
+        <div>
+          <p>
+            The king cobra was described and drawn by the Danish naturalist
+            Theodore Edward Cantor in 1836, who gave it the scientific name
+            Hamadryas hannah. Cantor had three specimens from the Sundarbans and
+            one caught in the vicinity of Kolkata. It was subordinated to the
+            genus Ophiophagus by Albert Günther in 1864.
+          </p>
+          <p>
+            Ophiophagus hannah belongs to the monotypic genus Ophiophagus in the
+            family Elapidae, while most other cobras are members of the genus
+            Naja. They can be distinguished from other cobras by size and hood.
+            King cobras are generally larger than other cobras, and the stripe
+            on the neck is a chevron instead of a double or single eye shape
+            that may be seen in most of the other Asian cobras. Moreover, the
+            hood of the king cobra is narrower and longer. A key to
+            identification, clearly visible on the head, is the presence of a
+            pair of large scales known as occipitals, located at the back of the
+            top of the head. These are behind the usual nine-plate arrangement
+            typical of colubrids and elapids, and are unique to the king cobra.
+          </p>
+          <p>
+            The skin of king cobra is dark olive or brown with black bands and
+            white or yellow crossbands. The head is black with two crossbars
+            near the snout and two behind the eyes. Adult king cobras are 3.18
+            to 4 m (10.4 to 13.1 ft) long. The longest known individual measured
+            5.85 m (19.2 ft). Its belly is cream or pale yellow. It has 17 to 19
+            rows of smooth scales. Ventral scales are uniformly oval shaped.
+            Dorsal scales are placed in an oblique arrangement. Males have 235
+            to 250 ventral scales, while females have 239 to 265. The subcaudal
+            scales are single or paired in each row, numbering 83 to 96 in males
+            and 77 to 98 in females.
+          </p>
+          <p>
+            Juveniles are shiny black with narrow yellow bands (can be mistaken
+            for a banded krait, but readily identified with its expandable
+            hood). The head of a mature snake can be quite massive and bulky in
+            appearance, though like all snakes, it can expand its jaws to
+            swallow large prey items. It has proteroglyph dentition, meaning it
+            has two short, fixed fangs in the front of the mouth, which channel
+            venom into the prey like hypodermic needles. The average lifespan of
+            a wild king cobra is about 20 years.
+          </p>
+        </div>
+      </article>
+      <article className="flex">
+        <div>
+          <div className="sticky top-8">Category Header</div>
+          <div className="sticky -z-[1] bottom-[100px]">
+            <p>Article 1 Footer</p>
+          </div>
+        </div>
+        <div>
+          <p>
+            The king cobra was described and drawn by the Danish naturalist
+            Theodore Edward Cantor in 1836, who gave it the scientific name
+            Hamadryas hannah. Cantor had three specimens from the Sundarbans and
+            one caught in the vicinity of Kolkata. It was subordinated to the
+            genus Ophiophagus by Albert Günther in 1864.
+          </p>
+          <p>
+            Ophiophagus hannah belongs to the monotypic genus Ophiophagus in the
+            family Elapidae, while most other cobras are members of the genus
+            Naja. They can be distinguished from other cobras by size and hood.
+            King cobras are generally larger than other cobras, and the stripe
+            on the neck is a chevron instead of a double or single eye shape
+            that may be seen in most of the other Asian cobras. Moreover, the
+            hood of the king cobra is narrower and longer. A key to
+            identification, clearly visible on the head, is the presence of a
+            pair of large scales known as occipitals, located at the back of the
+            top of the head. These are behind the usual nine-plate arrangement
+            typical of colubrids and elapids, and are unique to the king cobra.
+          </p>
+          <p>
+            The skin of king cobra is dark olive or brown with black bands and
+            white or yellow crossbands. The head is black with two crossbars
+            near the snout and two behind the eyes. Adult king cobras are 3.18
+            to 4 m (10.4 to 13.1 ft) long. The longest known individual measured
+            5.85 m (19.2 ft). Its belly is cream or pale yellow. It has 17 to 19
+            rows of smooth scales. Ventral scales are uniformly oval shaped.
+            Dorsal scales are placed in an oblique arrangement. Males have 235
+            to 250 ventral scales, while females have 239 to 265. The subcaudal
+            scales are single or paired in each row, numbering 83 to 96 in males
+            and 77 to 98 in females.
+          </p>
+          <p>
+            Juveniles are shiny black with narrow yellow bands (can be mistaken
+            for a banded krait, but readily identified with its expandable
+            hood). The head of a mature snake can be quite massive and bulky in
+            appearance, though like all snakes, it can expand its jaws to
+            swallow large prey items. It has proteroglyph dentition, meaning it
+            has two short, fixed fangs in the front of the mouth, which channel
+            venom into the prey like hypodermic needles. The average lifespan of
+            a wild king cobra is about 20 years.
+          </p>
+        </div>
+      </article>
     </div>
   )
 }
