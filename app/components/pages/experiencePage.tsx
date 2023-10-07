4a0d5595e424d32d4515d9ca428e9456a3045568
Han Le <hanle.cs23@gmail.com>
2023-10-06 20:19:45 -0400

    Fixed space issue from resumeModal

--- app/components/pages/experiencePage.tsx
+++ app/components/pages/experiencePage.tsx
@@ -28,40 +28,44 @@ export default function ExperiencePage(): React.JSX.Element {
   return (
     <div className="justify-items-center grid text-sky-100 relative">
       <h2 className="text-center font-extrabold text-lg md:text-5xl mt-8 mb-10">{`Experiences`}</h2>
-      <div className="sticky justify-self-end top-20 md:right-28">
+      <div className="sticky -m-[80px] justify-self-end top-20 md:right-28">
         <BlockContainer>
           <button
             onClick={() => {
-              setOpen(true)
+              setOpen(!open)
             }}
           >
             <Image src={resumeLogo} width={50} alt="Resume" />
           </button>
         </BlockContainer>
       </div>
-      {experiences?.map((experience) => {
-        return (
-          <article className="flex w-8/12" key={experience.title}>
-            <BlockContainer className="flex p-3">
-              <div className=" items-center w-3/12 shrink-0">
-                <div className="sticky top-10">
-                  {`${
-                    months[experience.startDate.getMonth()]
-                  } ${experience.startDate.getFullYear()} — ${
-                    months[experience.endDate.getMonth()]
-                  } ${experience.endDate.getFullYear()}`}
+
+      <div className="top-[-100%] grid justify-items-center">
+        {experiences?.map((experience) => {
+          return (
+            <article className="flex w-8/12" key={experience.title}>
+              <BlockContainer className="flex p-3">
+                <div className=" items-center w-3/12 shrink-0">
+                  <div className="sticky top-10">
+                    {`${
+                      months[experience.startDate.getMonth()]
+                    } ${experience.startDate.getFullYear()} — ${
+                      months[experience.endDate.getMonth()]
+                    } ${experience.endDate.getFullYear()}`}
+                  </div>
+                  <div className="sticky -z-[1] bottom-[100px]" />
                 </div>
-                <div className="sticky -z-[1] bottom-[100px]" />
-              </div>
-              <div>
-                <h2 className="text-2xl font-semibold">{experience.title}</h2>
-                <h3>{experience.location}</h3>
-                <p>{experience.description}</p>
-              </div>
-            </BlockContainer>
-          </article>
-        )
-      })}
+                <div>
+                  <h2 className="text-2xl font-semibold">{experience.title}</h2>
+                  <h3>{experience.location}</h3>
+                  <p>{experience.description}</p>
+                </div>
+              </BlockContainer>
+            </article>
+          )
+        })}
+      </div>
+
       <ResumeModal open={open} onClose={setOpen} />
       <div className="sticky -z-[1] bottom-[100px]" />
     </div>
