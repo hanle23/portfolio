ea24b157cafa4fd7a790a0cbebbb283688517c2f
Han Le <hanle.cs23@gmail.com>
2024-02-09 15:02:33 -0500

    Add Dropdown button and menu"

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,35 +1,120 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import BlockContainer from './specialComponent/BlockContainer'
+import {
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+} from '@nextui-org/react'
 import { Context } from '@/app/components/appWrapper'
 import scrollToSection from '@/app/components/scrollComponents/scrollToSection'
 
 export default function NavBar(): React.JSX.Element {
-  const route = ['experience', 'projects', 'contact']
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuContent = ['experience', 'projects', 'contact']
+  const mainNavContent = ['experience', 'projects', 'contact']
   const context = useContext(Context)
 
   return (
-    <div
-      className={`flex justify-center space-x-1 sticky top-0 z-[1] lg:space-x-44 px-2.5 bg-nav-light bg-opacity-60 w-fit rounded-md shadow-lg text-main-light
-      `}
-    >
-      {route?.map((routePath: string) => {
-        return (
-          <BlockContainer key={routePath}>
-            <a
-              onClick={() => {
-                if (routePath === 'contact') {
-                  context?.setContactOpen(!context.contactOpen)
-                  return
-                }
-                scrollToSection(`${routePath}-section`)
-              }}
-              className="p-2.5 hover:cursor-none text-base font-bold relative flex justify-center rounded-lg"
+    <Navbar onMenuOpenChange={setIsMenuOpen} className="z-[1] rounded-lg w-fit">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+      </NavbarContent>
+      <NavbarContent
+        className="hidden sm:flex gap-4  text-text-light w-fit"
+        justify="center"
+      >
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+        {mainNavContent?.map((routePath: string) => {
+          return (
+            <NavbarItem key={routePath}>
+              <BlockContainer>
+                <a
+                  onClick={() => {
+                    if (routePath === 'contact') {
+                      context?.setContactOpen(!context.contactOpen)
+                      return
+                    }
+                    scrollToSection(`${routePath}-section`)
+                  }}
+                  className="p-2.5 hover:cursor-none text-base font-bold relative flex justify-center rounded-lg"
+                >
+                  <p>
+                    {routePath.charAt(0).toUpperCase() + routePath.slice(1)}
+                  </p>
+                </a>
+              </BlockContainer>
+            </NavbarItem>
+          )
+        })}
+      </NavbarContent>
+      <NavbarContent>
+        <Dropdown>
+          <NavbarItem className="hidden sm:flex text-text-light">
+            <DropdownTrigger>
+              <BlockContainer>
+                <button className="p-2.5 hover:cursor-none text-base font-bold relative flex justify-center rounded-lg">
+                  <p>Apps</p>
+                </button>
+              </BlockContainer>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="apps"
+            className="w-[340px] z-100"
+            itemClasses={{
+              base: 'gap-4',
+            }}
+          >
+            {menuContent.map((item, index) => {
+              return (
+                <DropdownItem key={index}>
+                  <Link
+                    onClick={() => {
+                      scrollToSection(`${item}-section`)
+                    }}
+                  >
+                    {item}
+                  </Link>
+                </DropdownItem>
+              )
+            })}
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuContent.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2
+                  ? 'warning'
+                  : index === menuContent.length - 1
+                  ? 'danger'
+                  : 'foreground'
+              }
+              href="#"
+              size="lg"
             >
-              <p>{routePath.charAt(0).toUpperCase() + routePath.slice(1)}</p>
-            </a>
-          </BlockContainer>
-        )
-      })}
-    </div>
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   )
 }
