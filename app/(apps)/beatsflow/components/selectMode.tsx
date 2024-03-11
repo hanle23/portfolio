a96196f9bac389aa5645aa8101b629ac6b38084a
Han Le <hanle.cs23@gmail.com>
2024-03-11 10:18:40 -0400

    finished select mode component"

--- app/(apps)/beatsflow/components/selectMode.tsx
+++ app/(apps)/beatsflow/components/selectMode.tsx
@@ -4,36 +4,34 @@ export default function SelectMode({
 }: {
   className: string
 }): React.JSX.Element {
-  const modes = [
-    { value: 'playlist', label: 'Playlist' },
-    { value: 'playlist1', label: 'Playlist1' },
-    { value: 'playlist2', label: 'Playlist2' },
-  ]
+  const modes = [{ value: 'playlist', label: 'Playlist' }]
   return (
     <div className={className}>
-      <Select
-        items={modes}
-        label="Select a mode"
-        defaultSelectedKeys={['playlist']}
-        className=""
-        disallowEmptySelection={true}
-        size="lg"
-        listboxProps={{
-          className: 'bg-[#27272a] text-white h-fit rounded-md',
-        }}
-        popoverProps={{ className: '' }}
-        selectionMode="single"
-        labelPlacement="inside"
-      >
-        {modes.map((mode) => (
-          <SelectItem
-            className="text-white hover:bg-[#3c3c3e]"
-            key={mode.value}
-          >
-            {mode.label}
-          </SelectItem>
-        ))}
-      </Select>
+      <div className="flex flex-col w-full h-full">
+        <div className="px-2.5">Select Mode:</div>
+        <Select
+          items={modes}
+          defaultSelectedKeys={['playlist']}
+          className="text-lg"
+          disallowEmptySelection={true}
+          size="lg"
+          listboxProps={{
+            className: 'bg-[#27272a] text-white h-fit rounded-md',
+          }}
+          selectionMode="single"
+          labelPlacement="inside"
+        >
+          {modes.map((mode) => (
+            <SelectItem
+              className="text-white hover:bg-[#3c3c3e]"
+              key={mode.value}
+            >
+              {mode.label}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+
       {/* <select className="flex w-full rounded-lg p-2.5 h-full bg-[#27272a]">
         {modes.map((mode) => (
           <option key={mode.value}>{mode.label}</option>
