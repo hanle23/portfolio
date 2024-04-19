450598690caf2a34a3651657daff516f15efa447
Han Le <73045899+hanle23@users.noreply.github.com>
2024-04-19 11:00:56 -0400

    Replaced NextUI with Material UI, changed navBar (#64)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
-import { nextui } from '@nextui-org/react'
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -18,7 +15,6 @@ module.exports = {
         'text-light': '#3a405c',
       },
     },
-    darkMode: 'class',
-    plugins: [nextui()],
+    plugins: [],
   },
 }
