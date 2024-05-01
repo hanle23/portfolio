0d44ab855fd0023f0bbf82fde4b598a8dc7f3d5f
Han Le <hanle.cs23@gmail.com>
2024-05-01 14:31:43 -0400

    Finished orches header

--- app/(apps)/orches/components/header.tsx
+++ app/(apps)/orches/components/header.tsx
@@ -39,67 +39,6 @@ export function Header({
   }
 
   return context?.profile !== null ? (
-    // <Navbar
-    //   className={className ?? 'h-[8%] w-full overflow-hidden'}
-    //   maxWidth="full"
-    // >
-    //   <NavbarContent justify="start" className="flex w-fit ">
-    //     <Link href={'/'} className="flex gap-2 items-center w-fit">
-    //       <Image src={leftarrow} alt="left arrow" width={18} />
-    //       Back to portfolio
-    //     </Link>
-    //   </NavbarContent>
-    //   <NavbarContent justify="center" className="w-fit">
-    //     <Link href={'/orches'} className="text-2xl font-bold">
-    //       Beats Flow
-    //     </Link>
-    //   </NavbarContent>
-    //   <NavbarContent as="div" justify="end" className="w-full ">
-    //     <Dropdown
-    //       placement="bottom-end"
-    //       className="bg-[#282828] text-white text-sm rounded-md"
-    //     >
-    //       <DropdownTrigger>
-    //         <Avatar
-    //           isBordered
-    //           as="button"
-    //           className="transition-transform ring-spotify-color"
-    //           name={context?.profile?.display_name}
-    //           size="sm"
-    //           src={
-    //             context?.profile?.images?.find((img) => img.width === 300)?.url
-    //           }
-    //         />
-    //       </DropdownTrigger>
-    //       <DropdownMenu aria-label="Profile Actions" variant="flat">
-    //         <DropdownItem
-    //           key="profile"
-    //           className="h-fit"
-    //           textValue="Sign in as"
-    //         >
-    //           <Link
-    //             className="flex flex-col gap-1 items-start"
-    //             href={'/orches/profile'}
-    //           >
-    //             <p className="font-semibold">Signed in as</p>
-    //             <Suspense fallback={<div>loading...</div>}>
-    //               <p className="font-semibold">
-    //                 {context?.profile?.display_name}
-    //               </p>
-    //             </Suspense>
-    //           </Link>
-    //         </DropdownItem>
-    //         <DropdownItem
-    //           key="logout"
-    //           className="text-red-600"
-    //           onClick={handleLogout}
-    //         >
-    //           Log Out
-    //         </DropdownItem>
-    //       </DropdownMenu>
-    //     </Dropdown>
-    //   </NavbarContent>
-    // </Navbar>
     <AppBar
       position="absolute"
       color="transparent"
@@ -189,21 +128,6 @@ export function Header({
             </MenuList>
           </Menu>
         </Box>
-        {/* {mainNavContent.map((item) => (
-              <BlockContainer
-                key={item}
-                className="text-text-light font-bold p-2.5"
-                onClick={() => {
-                  if (item === 'contact') {
-                    context?.setContactOpen(!context.contactOpen)
-                    return
-                  }
-                  scrollToSection(`${item}-section`)
-                }}
-              >
-                {item.charAt(0).toUpperCase() + item.slice(1)}
-              </BlockContainer>
-            ))} */}
       </Toolbar>
     </AppBar>
   ) : (
