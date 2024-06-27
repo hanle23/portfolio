94458113775358a9909ea48a19ae0c3c90cace2a
Han Le <hanle.cs23@gmail.com>
2024-06-26 20:27:11 -0400

    Added greeting

--- app/(portfolio)/components/greetingPhrase.tsx
+++ app/(portfolio)/components/greetingPhrase.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect'
 export default function GreetingPhrase(): React.JSX.Element {
-  const phrases = ["Hi, I'm Han", '안녕하세요 한이에요']
+  const phrases = [
+    "Hi, I'm Han",
+    '안녕하세요 한이에요',
+    'Chào bạn, mình là Hiếu',
+  ]
 
   return (
     <div className="h-fit w-fit text-3xl lg:text-7xl font-bold">
