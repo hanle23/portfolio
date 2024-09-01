3302ad4d3f60805ae195205b23287cb278feff3e
Han Le <hanle.cs23@gmail.com>
2024-09-01 01:42:19 -0400

    Remove unused fragment

--- app/components/pages/projectPage/projectPageContent.tsx
+++ app/components/pages/projectPage/projectPageContent.tsx
@@ -10,30 +10,28 @@ export default function ProjectPageContent({
 }): JSX.Element {
   const Content = ({ isPrivate }: { isPrivate: boolean }): JSX.Element => {
     return (
-      <>
-        <div className="flex flex-col h-full overflow-hidden">
-          <div className=" font-bold text-lg md:text-xl">
-            {`${project.name
-              .replace(/-/gi, ' ')
-              .replace(/(^\w|\s\w)/g, (m: string) =>
-                m.toUpperCase(),
-              )}${isPrivate ? ' (Private)' : ''}`}
-          </div>
-          <p className="line-clamp-2">{project.description ?? ''}</p>
-          <div className="text-sm  flex items-center space-x-1 mt-auto">
-            <div
-              className={`border border-transparent ${
-                maxDate.getTime() <= currentDate ? 'bg-sky-100' : 'bg-green-600'
-              } h-[10px] w-[10px] rounded-full`}
-            />
-            <div>
-              {maxDate.getTime() <= currentDate
-                ? 'Stale'
-                : 'New update available!'}
-            </div>
+      <div className="flex flex-col h-full overflow-hidden">
+        <div className=" font-bold text-lg md:text-xl">
+          {`${project.name
+            .replace(/-/gi, ' ')
+            .replace(/(^\w|\s\w)/g, (m: string) =>
+              m.toUpperCase(),
+            )}${isPrivate ? ' (Private)' : ''}`}
+        </div>
+        <p className="line-clamp-2">{project.description ?? ''}</p>
+        <div className="text-sm  flex items-center space-x-1 mt-auto">
+          <div
+            className={`border border-transparent ${
+              maxDate.getTime() <= currentDate ? 'bg-sky-100' : 'bg-green-600'
+            } h-[10px] w-[10px] rounded-full`}
+          />
+          <div>
+            {maxDate.getTime() <= currentDate
+              ? 'Stale'
+              : 'New update available!'}
           </div>
         </div>
-      </>
+      </div>
     )
   }
   return project?.private ? (
